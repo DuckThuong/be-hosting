128e88e81e909f8eb77d4df76c6ceb861c0f1c64
Thuong <thuongtrinhduc@tsoft.vn>
2026-01-27 17:38:59 +0700

    fix: auth dto

--- src/dtos/auth/signIn.dto.ts
+++ src/dtos/auth/signIn.dto.ts
@@ -1,8 +1,21 @@
-export interface SignInPayload {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SignInPayload {
+  @ApiProperty({
+    description: 'Email người dùng',
+    example: 'user@example.com',
+    required: true,
+  })
   email: string;
+
+  @ApiProperty({
+    description: 'Mật khẩu người dùng',
+    example: 'P@ssw0rd!',
+    required: true,
+  })
   password: string;
 }
 
-export interface SignInDtoResponse {
+export class SignInDtoResponse {
   access_token: string;
 }
--- src/dtos/auth/signUp.dto.ts
+++ src/dtos/auth/signUp.dto.ts
@@ -1,9 +1,28 @@
-export interface SignUpPayload {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SignUpPayload {
+  @ApiProperty({
+    description: 'Tên người dùng',
+    example: 'User ....',
+    required: true,
+  })
   userName: string;
+
+  @ApiProperty({
+    description: 'Email người dùng',
+    example: 'user@example.com',
+    required: true,
+  })
   email: string;
+
+  @ApiProperty({
+    description: 'Mật khẩu người dùng',
+    example: 'P@ssw0rd!',
+    required: true,
+  })
   password: string;
 }
 
-export interface SignUpDtoResponse {
+export class SignUpDtoResponse {
   message: string;
 }
