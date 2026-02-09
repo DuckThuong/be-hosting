c6bf56cafd84d805c310a227bd0240c2b12f2e26
Thuong <thuongtrinhduc@tsoft.vn>
2026-02-09 09:54:32 +0700

    fix: build error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TbUserDefault } from './entities/user/user_default.entity';
 import { AuthModule } from './modules/auth.module';
@@ -15,34 +15,37 @@ import { CloudinaryModule } from './modules/cloudinary.module';
 
 @Module({
   imports: [
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
 
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQLHOST,
-      port: Number(process.env.MYSQLPORT),
-      username: process.env.MYSQLUSER,
-      password: process.env.MYSQLPASSWORD,
-      database: process.env.MYSQLDATABASE,
-
-      autoLoadEntities: true,
-      synchronize: process.env.TYPEORM_SYNC === 'true',
-      logging: process.env.TYPEORM_LOGGING === 'true',
-      entities: [
-        TbUserDefault,
-        TbUserProfile,
-        TbService,
-        TbLocation,
-        TbLocationService,
-        TbLocationType,
-      ],
-      migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-      migrationsRun: false,
+    // Đổi từ forRoot() sang forRootAsync()
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService], // Inject ConfigService
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('MYSQLHOST'),
+        port: configService.get<number>('MYSQLPORT'),
+        username: configService.get('MYSQLUSER'),
+        password: configService.get('MYSQLPASSWORD'),
+        database: configService.get('MYSQLDATABASE'),
+        autoLoadEntities: true,
+        synchronize: configService.get('TYPEORM_SYNC') === 'true',
+        logging: configService.get('TYPEORM_LOGGING') === 'true',
+        entities: [
+          TbUserDefault,
+          TbUserProfile,
+          TbService,
+          TbLocation,
+          TbLocationService,
+          TbLocationType,
+        ],
+        migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+        migrationsRun: false,
+      }),
     }),
+
     AuthModule,
     ServiceModule,
     LocationModule,
